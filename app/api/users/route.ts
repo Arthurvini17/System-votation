c6f46b2e54385caec5db3fe2898f3779162ee270
Arthur <arthurdedsec@gmail.com>
2026-02-04 15:26:52 -0300

    Feat: adicionado rotas da api rest

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,20 +1,18 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-
 export async function GET() {
-    const users = await prisma.user.findMany();
-    return NextResponse.json(users);
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
-
 export async function POST(req: Request) {
-    const { email, password, name } = await req.json();
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password,
-        },
-    });
-    return NextResponse.json(user);
+  const { email, password, name } = await req.json();
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  });
+  return NextResponse.json(user);
 }
